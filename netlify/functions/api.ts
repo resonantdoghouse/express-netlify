0acd3270c2b6f8ea959d0664b925d97f38d09d99
Jim <jim@advancebass.com>
2024-09-09 16:59:40 -0700

    add joke by id route

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,13 +1,16 @@
-import express, { Router } from 'express';
-import serverless from 'serverless-http';
-import jokesData from '../../data/jokes.json';
+import express, { Router } from "express";
+import serverless from "serverless-http";
+import jokesData from "../../data/jokes.json";
 
 const api = express();
 
 const router = Router();
-router.get('/hello', (req, res) => res.send('Hello World!'));
-router.get('/jokes', (req, res) => res.json(jokesData));
+router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/jokes", (req, res) => res.json(jokesData));
+router.get("/jokes/:id", (req, res) => {
+  res.json(jokesData.find((joke) => joke.id === req.params.id));
+});
 
-api.use('/api/', router);
+api.use("/api/", router);
 
 export const handler = serverless(api);
